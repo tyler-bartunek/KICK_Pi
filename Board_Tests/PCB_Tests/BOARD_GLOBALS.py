afef7048fb1942041ce8d1f4123988701af42098
tyler-bartunek <tylerbartunek@gmail.com>
2026-04-01 17:52:07 -0600

    Refactor to use RPi.GPIO and spidev, confirmed basic picotest still works.

--- Board_Tests/PCB_Tests/BOARD_GLOBALS.py
+++ Board_Tests/PCB_Tests/BOARD_GLOBALS.py
@@ -3,17 +3,17 @@
 
 #SPI Pins
 SPI0_SCLK = 11
-CHANNEL = 0
 CIPO = 9
 COPI = 10
 CS = 8 #Default CS for the SPI channel
 
 SYNC_RATE = 32000
+CHANNEL = 0
 
 #Shift register pins
-LATCH = 5 
-DATA = 6
+LATCH = 6 
+DATA = 26
 SHIFT_CLK = 13
-OE = 26
+OE = 5
 
 
--- Board_Tests/PCB_Tests/ShiftRegister.py
+++ Board_Tests/PCB_Tests/ShiftRegister.py
@@ -1,20 +1,20 @@
-import pigpio
+import RPi.GPIO as GPIO
 
 class ShiftRegister:
     
-    def __init__(self, pi, data_pin:int, latch_pin:int, sck_pin:int, oe_pin:int):
+    def __init__(self, data_pin:int, latch_pin:int, sck_pin:int, oe_pin:int):
         
         self.data_pin = data_pin
         self.latch_pin = latch_pin
         self.sck_pin = sck_pin
         self.oe_pin = oe_pin
         
-        #Configure pigpio object to control gpio pins
-        self.pi = pi
+        #Configure GPIO pins
+        GPIO.setmode(GPIO.BCM)
         self.connect_pins()
 
         #Disable outputs by default until our first write
-        self.pi.write(self.oe_pin, 1)
+        GPIO.output(self.oe_pin, 1)
 
         #index variable for data bitarray and flag to mark when message is sent
         self.bit_index = 0
@@ -27,25 +27,25 @@ class ShiftRegister:
         self.data_list = self.to_bitarray(data)
         
         #Pull latch low for shift
-        self.pi.write(self.latch_pin, 0)
+        GPIO.output(self.latch_pin, 0)
         
         #Send data bit by bit to the shift register
         for bit in self.data_list:
-            self.pi.write(self.data_pin, bit)
-            self.pi.write(self.sck_pin, 1)
-            self.pi.write(self.sck_pin, 0)
+            GPIO.output(self.data_pin, bit)
+            GPIO.output(self.sck_pin, 1)
+            GPIO.output(self.sck_pin, 0)
 
         #Enable outputs, shift values to storage register and outputs
-        self.pi.write(self.latch_pin, 1)
-        self.pi.write(self.oe_pin, 0)
+        GPIO.output(self.latch_pin, 1)
+        GPIO.output(self.oe_pin, 0)
 
     
     def connect_pins(self) -> None:
-        
-        self.pi.set_mode(self.data_pin, pigpio.OUTPUT)
-        self.pi.set_mode(self.latch_pin, pigpio.OUTPUT)
-        self.pi.set_mode(self.sck_pin, pigpio.OUTPUT)
-        self.pi.set_mode(self.oe_pin, pigpio.OUTPUT)
+
+        GPIO.setup(self.data_pin, GPIO.OUT)
+        GPIO.setup(self.latch_pin, GPIO.OUT)
+        GPIO.setup(self.sck_pin, GPIO.OUT)
+        GPIO.setup(self.oe_pin, GPIO.OUT)
 
         return None
             
