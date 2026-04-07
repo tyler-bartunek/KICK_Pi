7f814d09df3173ddb0cc626063e17b99a54b23e5
tyler-bartunek <tylerbartunek@gmail.com>
2026-04-07 11:26:25 -0600

    Add battery monitoring code template, fault detection prototype

--- sros_ws/src/battery_monitor/battery_monitor/battery_monitor_node.py
+++ sros_ws/src/battery_monitor/battery_monitor/battery_monitor_node.py
@@ -1,24 +1,35 @@
 
 #TODO: Import ADS monitoring library
+import ADS1x15
 
 #ROS stuff
 import rclpy
 from rclpy.node import Node
 
-#TODO: Import relevant message type for battery-info topic
+#Import relevant message type for battery-info topic
+from battery_monitor.msg import battery_info
 
 class BatteryMonitor(Node):
 
     def __init__(self):
         super().__init__('battery-monitor')
         #TODO: Create publisher to battery-info topic, decide on message format
-        #self.publisher_ = self.create_publisher(<insert type>, 'battery-info', 10)
-        raise NotImplementedError
+        self.publisher_ = self.create_publisher(battery_info, 'battery-info', 10)
+        self.pin_to_read = 0
+        timer_freq = 10.0 #Hz
+        timer_period = 1.0 / timer_freq
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
 
-        raise NotImplementedError
+        ads.requestADC(self.pin_to_read)
+        value = ads.getValue()
 
+        #TODO: Convert value to voltage and package into message, publish to topic
+        voltage = ads.toVoltage(value)
+        msg = battery_info()
+        msg.voltage = voltage
+        self.publisher_.publish(msg)
 
 def main():
 
--- sros_ws/src/spi_driver/spi_driver/bus_hub_node.py
+++ sros_ws/src/spi_driver/spi_driver/bus_hub_node.py
@@ -6,7 +6,7 @@ from rclpy.node import Node
 #Import dependencies from hardware_interfaces
 from hardware_interfaces import DeviceInterface, Harness
 
-from spi_driver.msg import SPIFrame, cmdFrame
+from spi_driver.msg import SPIFrame, DeviceData
 
 class BusManager():
 
@@ -24,16 +24,19 @@ class BusManager():
         discovery_rate = 32000
 
         #Perform the handshake
-        handshake_message = [0xFE, 0xFF, 0x00, 0x00, 0x7F]
+        path_header = 0xF | (path_id & 0x7) #First four bits are 1111, rest are path_id
+        handshake_data = [path_header, 0xFF, 0x00, 0x00]
+        checksum = self.compute_checksum(handshake_data)
+        handshake_message = handshake_data + [checksum, 0xCF] #add checksum and padding that prevents bit-stuffing
         response = self.spi.transfer(path_id, handshake_message, self.channel, discovery_rate) #Send handshake_message and get response
 
         #Disable the bus, freeing it for other activities
         self.spi.disable_bus()
 
-        #TODO: Decide what success looks like
-        expected = []
-        if response == expected:
-            return response[1] #Returns the identifier for the module
+        #Expected it to return padding, path_header, the device_id, data, and then the checksum
+        expected = [0xCF, path_header, 0x00, 0x00, 0x00, checksum]
+        if response[5] == expected[5]:
+            return response[2] #Returns the identifier for the module
         else:
             return None #No device on this path
 
@@ -48,9 +51,34 @@ class BusManager():
                 device.id = device_id
                 device.comms_rate = self.comms_rate
 
-    def detect_fault(self, response_data):
-        #TODO: Implement logic to detect if the response data indicates a fault
-        pass
+    def detect_fault(self, cmd_data:list, response_data:list, device):
+        #Extract path_id from cmd_data
+        path_id = cmd_data[0] & 0x7
+
+        #If the checksum fails, this could indicate a fault in the communication or the device. If the checksum is correct but the path_id or device_id doesn't match, this could indicate a mismatch in the expected response, which may also suggest a fault.
+        if response_data[5] != self.compute_checksum(cmd_data[:-1]):
+            self.node.get_logger().warn(f"Checksum failure detected in response: {response_data}")
+            #If the checksum is 0xFF but this is not expected for the response values, internal fault on device may be indicated
+            if self.compute_checksum(response_data[1:-2]) == 0xFF:
+                self.node.get_logger().warn(f"Device on path {path_id} may be experiencing internal fault")
+                device.status = "fault" #known fault state
+            return True
+
+        #If the checksum is correct but the path_id or device_id doesn't match, this could indicate a mismatch in the expected response, which may also suggest a fault.
+        elif response_data[1] != cmd_data[0]:
+            self.node.get_logger().warn(f"Path ID mismatch in response on path: {response_data, path_id}")
+            return True
+        elif response_data[2] != device.id:
+            self.node.get_logger().warn(f"Device ID mismatch in response on path: {response_data, path_id}")
+            return True
+        #If we pass these checks, the data is likely good.
+        else:
+            device.status = "active"
+            return False
+
+    def compute_checksum(self, data:list):
+        
+        return sum(data) % 256
 
     def poll_devices(self, cmd):
 
@@ -66,7 +94,7 @@ class BusManager():
                 if device.status == "active":
                     response = self.spi.transfer(path_id, cmd, device.channel, device.comms_rate)
                     #TODO: Package response into SPIFrame message
-                    if self.detect_fault(response):
+                    if self.detect_fault(cmd, response, device):
                         device.status = "fault"
                         self.node.get_logger().info(f"Fault detected on path {path_id}")
                     else:
